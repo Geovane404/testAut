919a66d5815c8cb30f82efa8c0e4fb24b0ef55cc
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-04 22:31:28 -0300

    project created

---
+++ src/main/java/com/gtecnologia/testAut/TestAutApplication.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.testAut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtecnologia/testAut/TestAutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.testAut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
