5d10cc15ef90dda2c795e8075c193cd72ea66b62
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-07 15:27:00 -0300

    Database seeding

--- src/main/java/com/gtecnologia/testAut/entities/Category.java
+++ src/main/java/com/gtecnologia/testAut/entities/Category.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
