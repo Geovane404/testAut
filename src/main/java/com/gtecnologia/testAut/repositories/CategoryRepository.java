65b48126003b80ce76854b9fe572fc9365fcd3ef
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-07 15:04:13 -0300

    h2 database

---
+++ src/main/java/com/gtecnologia/testAut/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.gtecnologia.testAut.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gtecnologia.testAut.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+
+	
+}
