ddbbb41895ed3b5240dd92c442b814c1737855b0
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-08 08:10:11 -0300

    Created new category

--- src/main/java/com/gtecnologia/testAut/services/CategoryService.java
+++ src/main/java/com/gtecnologia/testAut/services/CategoryService.java
@@ -34,6 +34,16 @@ public class CategoryService {
 		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado!"));
 		return new CategoryDTO(entity);
 	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		 entity = repository.save(entity);
+		 
+		return new CategoryDTO(entity);
+	}
 	
 
 }
