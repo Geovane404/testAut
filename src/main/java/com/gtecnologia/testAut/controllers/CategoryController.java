afb6ca01dffb03a64eed78aaeec6a27091338794
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-08 16:46:21 -0300

    Pagination

--- src/main/java/com/gtecnologia/testAut/controllers/CategoryController.java
+++ src/main/java/com/gtecnologia/testAut/controllers/CategoryController.java
@@ -1,9 +1,10 @@
 package com.gtecnologia.testAut.controllers;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,48 +20,47 @@ import com.gtecnologia.testAut.dto.CategoryDTO;
 import com.gtecnologia.testAut.services.CategoryService;
 
 @RestController
-@RequestMapping(value = "categories")
+@RequestMapping(value = "/categories")
 public class CategoryController {
-
+	
 	@Autowired
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findAll() {
-
-		List<CategoryDTO> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+		
+		Page<CategoryDTO> pagelist = service.findALLPaged(pageable);
+		return ResponseEntity.ok().body(pagelist);
 	}
-
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
-
-		CategoryDTO dto = service.fidById(id);
+		
+		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	@PostMapping
-	public ResponseEntity<CategoryDTO> insert( @RequestBody CategoryDTO dto) {
+	@PostMapping 
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
 		
 		dto = service.insert(dto);
-		URI uri =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(dto.getId()).toUri();
-		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
+	public ResponseEntity<CategoryDTO>update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
 		
-		dto = service.updade(id, dto);
+		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	@DeleteMapping(value = "{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id) {
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void>delete(@PathVariable Long id ) {
 		
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-
+	
 }
--- src/main/java/com/gtecnologia/testAut/services/CategoryService.java
+++ src/main/java/com/gtecnologia/testAut/services/CategoryService.java
@@ -1,14 +1,15 @@
 package com.gtecnologia.testAut.services;
 
-import java.util.List;
+
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,46 +21,45 @@ import com.gtecnologia.testAut.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
-
+	
 	@Autowired
 	private CategoryRepository repository;
-
+	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-
-		List<Category> list = repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	public Page<CategoryDTO> findALLPaged(Pageable pageable) {
+		
+		Page<Category>pagelist = repository.findAll(pageable);
+		return pagelist.map(x -> new CategoryDTO(x));
 	}
 
-	@Transactional
-	public CategoryDTO fidById(Long id) {
-
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado!"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Departamento não encontrado"));
 		return new CategoryDTO(entity);
 	}
 
 	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
-
+		
 		Category entity = new Category();
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
-
 		return new CategoryDTO(entity);
 	}
 
 	@Transactional
-	public CategoryDTO updade(Long id, CategoryDTO dto) {
+	public CategoryDTO update(Long id, CategoryDTO dto) {
 		
 		try {
-			Category entity = repository.getOne(id);
+			Category entity = repository.getOne(id );
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
-		} 
-		catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id não encontrado!");
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("id não encontrado " + id);
 		}
 	}
 
@@ -68,11 +68,10 @@ public class CategoryService {
 			repository.deleteById(id);
 		}
 		catch(EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException("Id não encontrado!");
+			throw new ResourceNotFoundException("Id não encontrado");
 		}
 		catch(DataIntegrityViolationException e) {
-			throw new DatabaseException("Violação de integridade! você não pode excluir um departamento que não esta vazio"); 
+			throw new DatabaseException("Violação de integridade! você não pode excluir um departamento que não esta vazio");
 		}
 	}
-
 }
