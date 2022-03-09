43c916e11c4f1bc65a4726e6d92d4db66c23726f
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-08 21:54:10 -0300

    Product insert, update, delete

--- src/main/java/com/gtecnologia/testAut/services/ProductService.java
+++ src/main/java/com/gtecnologia/testAut/services/ProductService.java
@@ -13,8 +13,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gtecnologia.testAut.dto.CategoryDTO;
 import com.gtecnologia.testAut.dto.ProductDTO;
+import com.gtecnologia.testAut.entities.Category;
 import com.gtecnologia.testAut.entities.Product;
+import com.gtecnologia.testAut.repositories.CategoryRepository;
 import com.gtecnologia.testAut.repositories.ProductRepository;
 import com.gtecnologia.testAut.services.exceptions.DatabaseException;
 import com.gtecnologia.testAut.services.exceptions.ResourceNotFoundException;
@@ -24,6 +27,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findALLPaged(Pageable pageable) {
@@ -44,9 +50,9 @@ public class ProductService {
 	public ProductDTO insert(ProductDTO dto) {
 		
 		Product entity = new Product();
-		entity.setName(dto.getName());
+		copyDTO(entity, dto);
 		entity = repository.save(entity);
-		return new ProductDTO(entity);
+		return new ProductDTO( entity);
 	}
 
 	@Transactional
@@ -54,7 +60,7 @@ public class ProductService {
 		
 		try {
 			Product entity = repository.getOne(id );
-			entity.setName(dto.getName());
+			copyDTO(entity, dto);
 			entity = repository.save(entity);
 			return new ProductDTO(entity);
 		}
@@ -74,4 +80,22 @@ public class ProductService {
 			throw new DatabaseException("Violação de integridade! você não pode excluir um departamento que não esta vazio");
 		}
 	}
+	
+	//METODO PARA FASER COPIA PARA INSERTE E UPDATE
+	public void copyDTO(Product entity, ProductDTO dto) {
+		
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setDate(dto.getDate());
+		
+		entity.getCategories().clear();
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
+		
+	}
+	
 }
