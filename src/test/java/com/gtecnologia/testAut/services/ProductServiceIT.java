59150a1906fbd3ecbb722ec4ad7e523a2e1255f8
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-13 15:11:40 -0300

    ProductServiceIT - FindAllPaged and FinById

--- src/test/java/com/gtecnologia/testAut/services/ProductServiceIT.java
+++ src/test/java/com/gtecnologia/testAut/services/ProductServiceIT.java
@@ -5,54 +5,109 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gtecnologia.testAut.dto.ProductDTO;
 import com.gtecnologia.testAut.repositories.ProductRepository;
 import com.gtecnologia.testAut.services.exceptions.ResourceNotFoundException;
 
-
 //TESTES || INTEGRAÇÃO || VALIDAR INTEGRAÇÃO DE COMPONENTES || SERVICE <---> REPOSITOTY
-
 
 @SpringBootTest
 @Transactional
 public class ProductServiceIT {
-	
+
 	@Autowired
 	private ProductService service;
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
+
 	private long existingId;
 	private long nonExistingId;
 	private long countTotalProduct;
-	
-	
-	//FIXTURES
+
+	// FIXTURES
 	@BeforeEach
 	void setUp() throws Exception {
-		
+
 		existingId = 1L;
 		nonExistingId = 1000L;
 		countTotalProduct = 25L;
+
+	}
+
+	// ---TESTES PARA VALIDAR BUSCAS:
+	@Test
+	public void findALLPagedShouldReturnSortedPagewhenSortByName() {
+
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+
+		Page<ProductDTO> result = service.findALLPaged(pageRequest);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+	}
+
+	@Test
+	public void findALLPagedShouldReturnPagewhenPage0Size10() {
+		PageRequest pageRequest = PageRequest.of(0, 10);
+
+		Page<ProductDTO> result = service.findALLPaged(pageRequest);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(countTotalProduct, result.getTotalElements());
+	}
+
+	@Test
+	public void findALLPagedShouldReturnEmptyPagewhenPageDoesNotExist() {
+		PageRequest pageRequest = PageRequest.of(100, 10);
+
+		Page<ProductDTO> result = service.findALLPaged(pageRequest);
+
+		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void findByIdShouldReturnProductDTOWhenIdExist() {
+		
+		ProductDTO result = service.findById(existingId);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(existingId, result.getId());
+	}
+	
+	@Test
+	public void findByIdShouldThrowResourceNotFoundExceptionWhenIdNoExist() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.findById(nonExistingId);
+		});
 		
 	}
 	
 	// ---TESTES PARA VALIDAR DELEÇÕES:
 	@Test
-	public void deleteShouldDeleteResourceWhenIdExist () {
-		
-		 service.delete(existingId);
-		 Assertions.assertEquals(countTotalProduct - 1, repository.count());
+	public void deleteShouldDeleteResourceWhenIdExist() {
+
+		service.delete(existingId);
+		Assertions.assertEquals(countTotalProduct - 1, repository.count());
 	}
-	
+
 	@Test
 	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
-		
+
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.delete(nonExistingId);
 		});
 	}
 }
-
