67fd5948f8e81b1fa146054f5f3a002113e8d37c
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-03-13 21:46:29 -0300

    ProductControllerIT - FindAll and FindById

---
+++ src/test/java/com/gtecnologia/testAut/controller/ProductControllerIT.java
@@ -0,0 +1,89 @@
+package com.gtecnologia.testAut.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+//TESTES || INTEGRAÇÃO || VALIDAR INTEGRAÇÃO DE COMPONENTES || CONTROLLER<--->SERVICE <---> REPOSITOTY
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductControllerIT {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	private long  existingId;
+	private long nonExistingId;
+	private long countTotalProduct;
+
+	// FIXTURES
+	@BeforeEach
+	void setUp() throws Exception {
+
+		 existingId = 1L;
+		 nonExistingId = 1000L;
+		countTotalProduct = 25L;
+
+	}
+
+	// ---TESTES PARA VALIDAR BUSCAS:
+	@Test
+	public void findAllShouldReturnPage() throws Exception{
+		
+		ResultActions result = mockMvc.perform(get("/products")
+				.accept(MediaType.APPLICATION_JSON));
+				
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.content").exists());
+	}
+	
+	@Test
+	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+
+		ResultActions result = mockMvc
+				.perform(get("/products?page=0&size=12&sort=name,asc")
+						.accept(MediaType.APPLICATION_JSON));
+
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.totalElements").value(countTotalProduct));
+		result.andExpect(jsonPath("$.content").exists());
+		result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+		result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+		result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+	}
+	
+	@Test 
+	public void findByIdShouldReturnProductDTOWhenIdExists() throws Exception {
+		
+		ResultActions result = mockMvc.perform(get("/products/{id}", existingId)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.name").exists());
+		result.andExpect(jsonPath("$.description").exists());
+		result.andExpect(jsonPath("$.price").exists());
+	}
+	
+	@Test 
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+		
+		ResultActions result = mockMvc.perform(get("/products/{id}", nonExistingId)
+				.accept(MediaType.APPLICATION_JSON));
+		
+			result.andExpect(status().isNotFound());
+	}
+
+}
